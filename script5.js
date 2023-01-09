cd599e958d72ab8d6d7d952eba9990c2db27ebfa
MaAnka324 <115251389+MaAnka324@users.noreply.github.com>
2023-01-09 14:43:40 +0200

    Update script5.js

--- script5.js
+++ script5.js
@@ -28,3 +28,15 @@ function geo_mean(nums, arith_mean) {
   let ar = Math.pow((q*n), 1/(nums.length + 1));
   return ar
 }
+
+
+///////////////////////
+const rps = (p1, p2) => {
+  if(p1 === "scissors" && p2 === "paper") return "Player 1 won!"
+  if(p1 === "scissors" && p2 === "rock") return "Player 2 won!"
+  if(p1 === "paper" && p2 === "rock") return "Player 1 won!"
+  if(p1 === "paper" && p2 === "scissors") return "Player 2 won!"
+  if(p1 === "rock" && p2 === "scissors") return "Player 1 won!"
+  if(p1 === "rock" && p2 === "paper") return "Player 2 won!"
+  if(p1 === p2)return "Draw!"
+};
